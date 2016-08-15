6cd3ce8415a38fbd66c25db33c28f3529908f7a8
KUCKLU <kuck1u@visualive.jp>
2016-08-16 02:34:44 +0900

    no message

--- functions.php
+++ functions.php
@@ -129,7 +129,7 @@ add_action( 'widgets_init', 'nekodono_widgets_init' );
 function nekodono_scripts() {
 	wp_enqueue_style( 'nekodono-style', get_template_directory_uri() . '/assets/css/style.min.css' );
 
-	wp_enqueue_script( 'nekodono-apps', get_template_directory_uri() . '/assets/js/apps.js', array('jquery'), null, true );
+	wp_enqueue_script( 'nekodono-apps', get_template_directory_uri() . '/assets/js/apps.min.js', array('jquery'), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
